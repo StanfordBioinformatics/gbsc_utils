62f781795480fddcb756e9ee82d96f3567959443
nathankw <nathankw@stanford.ed>
2015-01-08 17:31:25 -0800

    Updated FASTA parser

--- fasta/fasta.py
+++ fasta/fasta.py
@@ -1,6 +1,15 @@
 
 import sys
 
+def getFastaIdFromHeader(header):
+	"""
+	Function : Parses out the FASTA record ID from the passed in header-line. The ID is parses as the first white-space delimited field in the header line.
+	Args     : header - str.
+	Returns  : str.
+	"""
+	header = header.lstrip(">")
+	return header.split()[0]
+
 class ByteIndex:
 	def __init__(self,infile):
 		self.fh = open(infile,'r')
@@ -12,42 +21,46 @@ class ByteIndex:
 		prevTell = 0
 		while 1:
 			line =self.fh.readline()
-			if not line:
+			if not line: #then end of file
 				break
 			line = line.strip()
 			curtell = self.fh.tell()
 			if line.startswith(">"):
-				recName = ByteIndex.getFastaId(line)
+				recName = getFastaIdFromHeader(line)
 				starts.append([recName,prevTell])
 			prevTell = curtell
 		for i in range(len(starts) - 1):
 			recname = starts[i][0]
 			recByteDict[recname] = [starts[i][1]] #start byte
-			recByteDict[recname].append(starts[i + 1][1] - 1)	#end byte
+			recByteDict[recname].append(starts[i + 1][1] - 1)	#end byte (lands on the newline character of the last line of the record in question)
 		self.fh.seek(0,2) #go to end of file
 		lastByte = self.fh.tell()
 		lastRecName = starts[-1][0]
 		recByteDict[lastRecName] = [starts[-1][1],lastByte]
 		return recByteDict
 	
-	@staticmethod
-	def getFastaId(header):
-		header = header.lstrip(">")
-		return header.split()[0]
-
 	def getRawRecord(self,name):
+		"""
+		Function : Retrieves the raw FASTA record with any leading and trailing white-space removed.
+		Args     : name - str. Name of the FASTA record.
+		Returns  : str. 
+		"""
 		recCoords = self.recBytes[name]
 		start = recCoords[0]
-		length = recCoords[-1] - start
+		length = recCoords[-1] - start + 1
 		self.fh.seek(start)
 		return self.fh.read(length).strip()
 
 class Rec:
 	def __init__(self,fastaRec):
 		self.rec  = fastaRec.split("\n")
-		self.header = self.rec[0]
+		self.header = self.rec[0].strip()
 		self.name = self.header.lstrip(">").split()[0]
-		self.seq = "".join(self.rec[1:]).upper()
+		seqList = [ x.strip() for x in self.rec[1:] ]
+		self.seq = "".join(seqList).upper()
+
+	def getName(self):
+		return self.name
 
 	def getSeq(self):
 		return self.seq
@@ -55,8 +68,11 @@ class Rec:
 	def getHeader(self):
 		return self.header
 
-	def printRecord(self,name,numChars):
-		bins = list(range(0,len(self.getSeq()) + numChars,numChars))
+	def printRecord(self,numCharsPerLine=70):
+		"""
+		Function : Prints out a sequence in chunks of size numCharsPerLine for each line.
+		"""
+		bins = list(range(0,len(self.getSeq()) + numCharsPerLine,numCharsPerLine))
 		print(self.getHeader())
 		for i in range(len(bins) - 1):
 #			print bins[i],bins[i + 1]
