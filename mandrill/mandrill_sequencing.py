887e5b90fc0b14bce693bd1be1e893aa23f1311a
Nathaniel Watson <nathan.watson86@gmail.com>
2015-09-08 13:17:11 -0700

    Adde newline

--- mandrill/mandrill_sequencing.py
+++ mandrill/mandrill_sequencing.py
@@ -47,7 +47,7 @@ parser.add_argument('--dry-run',action="store_true",help="Presence of this optio
 args = parser.parse_args()
 dryRun = args.dry_run
 verbose = args.verbose
-addText = "Notice: The mapping is now using the latest version 0.7.12 of BWA. This version doesn't include our custom option to map only a certain number of bases. As a result, the entire length of the reads are used in the mapping.\n\n"
+addText = "Notice: The mapping is now using the latest version 0.7.12 of BWA. This version doesn't include our custom option to map only a certain number of bases. As a result, the entire length of the reads are used in the mapping.\n\n\n"
 if args.add:
 	addText += args.add
 
