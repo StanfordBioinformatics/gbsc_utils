a086bc1613c4093bd69c8b9ec97fc807877807f2
Nathaniel Watson <nathankw@stanford.edu>
2015-10-21 17:50:46 -0700

    Added more test cases

--- illumina/illumina_fastq_file.py
+++ illumina/illumina_fastq_file.py
@@ -12,16 +12,18 @@ class FastqFile:
 		Args : fqfile - A FASTQ file name as assigned/formatted the Illumina demultiplexer. 
 		"""
 		self.fqf = fqfile
-		self.readNum = self._getReadNumber()
-		self.laneNum = self._getLaneNumber()
-		self.setNumber = self._getSetNumber()
+		self.tokens = self.fqf.split("_")
+		self._setReadNumber()   #self.read
+		self._setLaneNumber()   #self.lane
+		self._setSetNumber()    #self.set
+		self._setSampleId()      #self.id
 
 	def isForwardReadFile(self):
 		"""
 		Function : Reports whether the FASTQ file contains forward reads only.
 		Returns  : bool.
 		"""
-		if self.readNum == self.FORWARD_READ_NUM:
+		if self.read == self.FORWARD_READ_NUM:
 			return True
 		return False
 	
@@ -30,42 +32,46 @@ class FastqFile:
 		Function : Reports whether the FASTQ file contains reverse reads only.
 		Returns  : bool.
 		"""
-		if self.readNum == self.REVERSE_READ_NUM:
+		if self.read == self.REVERSE_READ_NUM:
 			return True
 		return False
 			
-	def _getReadNumber(self):
+	def _setReadNumber(self):
 		"""
 		Function : According to the v1.8.4 UG, bc2fastq names FASTQ files like so:
 					 		 <sample name>_<barcode sequence>_L<lane>_R<read number>_<set number>.fastq.gz	
 		Returns: int. in the set [1,2], where 1 means forward read and 2 means reverse read.
 		"""
-		tokens = self.fqf.split("_")
 		#Since the SAMPLE_PROJECT and SAMPLE_ID field in bcl2fastq 1X, and additionally SAMPLE_NAME in bcl2fastq2, can contain underscores, then we must always use negative indexing to get
 		# a token for the chunck, read number, or lane.	
-		readNum = int(tokens[-2].lstrip("R"))
+		readNum = int(self.tokens[-2].lstrip("R"))
 		if readNum not in  self.READ_NUMS:
 			raise UnknownReadNumberException("Unkwown read number '{readNum}' in FASTQ file name '{name}'. Only the following read numbers are recognized: '{allowed}'.".format(readNum=readNum,name=self.fqf,allowed=self.READ_NUMS))
-		return readNum
+		self.read = readNum
 	
-	def _getLaneNumber(self):
+	def _setLaneNumber(self):
 		"""
 		Function : Grabs the lane number (i.e. 1) from the FASTQ file name that was assigned by the sequencer.
 		Returns  : int.
 		"""
-		tokens = self.fqf.split("_")
 		#Since the SAMPLE_PROJECT and SAMPLE_ID field in bcl2fastq 1X, and additionally SAMPLE_NAME in bcl2fastq2, can contain underscores, then we must always use negative indexing to get
 		# a token for the chunck, read number, or lane.	
-		laneNum = tokens[-3].lstrip("L")
-		return int(laneNum)
+		laneNum = self.tokens[-3].lstrip("L")
+		self.lane = int(laneNum)
 	
-	def _getSetNumber(self):
+	def _setSetNumber(self):
 		"""
 		Function : Grabs the set number of a FASTQ file.
 		Returns  : int.
 		""" 
-		tokens = self.fqf.split("_")
 		#Since the SAMPLE_PROJECT and SAMPLE_ID field in bcl2fastq 1X, and additionally SAMPLE_NAME in bcl2fastq2, can contain underscores, then we must always use negative indexing to get
 		# a token for the chunck, read number, or lane.	
-		setNumber = tokens[-1].split(".")[0]
-		return setNumber
+		setNumber = self.tokens[-1].split(".")[0]
+		self.set = setNumber
+
+	def _setSampleId(self):
+		"""
+		This parse out the 4th '_' delimited field from the right, in the file name. For bcl2fastq v1.8.4, this
+		will be the barcode sequence (I7, or I7-I5). For bcl2fastq v2.17, this will be the sample number (i.e S1).
+		"""
+		self.id = self.tokens[-4]
--- illumina/test/test_illumina_fastq_file.py
+++ illumina/test/test_illumina_fastq_file.py
@@ -21,33 +21,75 @@ the illumina_fastq_file.FastqFile class, a test on a file output from either dem
 class TestFastqFile_isForwardReadFile(unittest.TestCase):
 
 	def test_true(self):
-		n = FastqFile("S60_CAGATC_L001_R1_003.fastq.gz")
+		n = FastqFile("m15_CAGATC_L001_R1_003.fastq.gz")
 		self.assertTrue(n.isForwardReadFile())
 
 
 	def test_false(self):
-		n = FastqFile("S60_CAGATC_L001_R2_003.fastq.gz")
+		n = FastqFile("m15_CAGATC_L001_R2_003.fastq.gz")
 		self.assertFalse(n.isForwardReadFile())
 
 	def test_raises_UnknownReadNumberException(self):
-		name = "S60_CAGATC_L001_R3_003.fastq.gz"
+		name = "m15_CAGATC_L001_R3_003.fastq.gz"
 		self.assertRaises(UnknownReadNumberException,FastqFile,name)
+
 
 class TestFastqFile_isReverseReadFile(unittest.TestCase):
 
 	def test_true(self):
-		n = FastqFile("S60_CAGATC_L001_R2_003.fastq.gz")
+		n = FastqFile("m15_CAGATC_L001_R2_003.fastq.gz")
 		self.assertTrue(n.isReverseReadFile())
 
 
 	def test_false(self):
-		n = FastqFile("S60_CAGATC_L001_R1_003.fastq.gz")
+		n = FastqFile("m15_CAGATC_L001_R1_003.fastq.gz")
 		self.assertFalse(n.isReverseReadFile())
 
 	def test_raises_UnknownReadNumberException(self):
-		name = "S60_CAGATC_L001_R3_003.fastq.gz"
+		name = "m15_CAGATC_L001_R3_003.fastq.gz"
 		self.assertRaises(UnknownReadNumberException,FastqFile,name)
+
+
+class TestFastqFile_laneNumber(unittest.TestCase):
+	
+	def test_lane1(self):
+		n = FastqFile("m15_CAGATC_L001_R2_003.fastq.gz")
+		self.assertEqual(n.lane,1)
+
+	def test_lane2(self):
+		n = FastqFile("m15_CAGATC_L002_R2_003.fastq.gz")
+		self.assertEqual(n.lane,2)
+
+
+class TestFastqFile_setNumber(unittest.TestCase):
+	
+	def test_1(self):
+		n = FastqFile("m15_CAGATC_L001_R2_001.fastq.gz")	
+		self.assertEqual(n.set,"001")
+
+	def test_3(self):
+		n = FastqFile("m15_CAGATC_L001_R2_003.fastq.gz")	
+		self.assertEqual(n.set,"003")
+
+class TestFastqFile_sampleId(unittest.TestCase):
+	
+	def test_CAGATC(self):
+		n = FastqFile("m15_CAGATC_L001_R2_003.fastq.gz")
+		self.assertEqual(n.id,"CAGATC")
+
+	def test_CAGATC_TCGGAA(self):
+		"""
+		When barcode is CAGATC-TCGGAA, then that should be the sample id. 
+		"""
+		n = FastqFile("m15_CAGATC-TCGGAA_L001_R2_003.fastq.gz")
+		self.assertEqual(n.id,"CAGATC-TCGGAA")
+		
+	def test_bcl2fastq2_S3(self):
+		"""
+		When the FASTQ file is demultiplexed with bcl2fastq v2.17, then the sample ID should be S followed by a number.
+		"""
+		n = FastqFile("m15_S4_L001_R2_003.fastq.gz")
+		self.assertEqual(n.id,"S4")
 
 if __name__ == "__main__":
 	unittest.main(verbosity=2)
-
