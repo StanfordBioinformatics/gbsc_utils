fdd15a94e1efdfd657f8338e9fd72dbc9cb68f7a
Nathaniel Watson <nathankw@stanford.edu>
2016-02-02 19:25:41 -0800

    Added main workflow script

---
+++ single_cell_rna_seq/run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -eu
+
+###
+#Nathaniel Watson
+#2016-02-02
+#
+# This program requires one positional argument, being the output directory. It must exist, otherwise the program will cease to execute."
+#
+###
+
+module load jsonwf/current
+module load gbsc/gbsc_utils #exports GBSC_UTILS env. var.
+
+outdir=$1
+inputFile=$2
+
+if ! [[ -d ${outdir} ]]
+then
+	echo "The output directory provided as the first positional argument does not exist. Exiting."
+	exit 1
+fi
+
+#parse input file just to make sure it's conformable
+count=0
+while read read1 read2
+do
+	count=$(( $count + 1 ))
+	if [[ -z $read1 || -z $read2 ]]
+	then
+		echo "Error on line ${count} in input file ${inputFile}: There should not be any blank lines, and all lines must have two space delimited entries indicating the read1 FASTQ file and the read2 FASTQ file, respectively."
+		exit 1
+	fi
+done < ${inputFile}
+
+#prepare to load the genome
+load_genome_sjm=load_genome.sjm
+
+if [[ -f ${sjm} ]]
+then
+	echo "SJM file ${sjm} already exists. Delete file and run the program again. Exiting."
+	exit 1
+fi
+
+conf=${GBSC_UTILS}/single_cell_rna_seq/single_cell_rna_seq.json
+
+#Load the genome
+jsonWorkflow.py -c ${conf} --outdir=${outdir} --sjmfile=${load_genome_sjm} --disable-all-except star_load_genome --wait --run
+
+#prepare to map the samples from the input file
+map_samples_sjm=map_samples.sjm
+
+while read read1 read2
+do
+	jsonWorkflow.py -c ${conf} --outdir=${outdir} --sjmfile=${map_samples_sjm} --disable-all-except star_mapper
+done
+
+sjm -i --mail ${mailTo} ${map_samples_sjm}
+
+#unload the genome
+unload_genome_sjm=unload_genome.sjm
+jsonWorkflow.py -c ${conf} --outdir=${outdir} --sjmfile=${unload_genome_sjm} --disable-all-except star_unload_genome --wait --run
+
+
+
+
