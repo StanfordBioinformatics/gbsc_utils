2903c23ae7c63e1e5641e24ad6afdc30c5aa737e
Nathaniel Watson <nathankw@stanford.edu>
2016-03-28 00:30:27 -0700

    updates

--- bwa_pipeline/run.py
+++ bwa_pipeline/run.py
@@ -7,7 +7,7 @@ MAIL_TO = "nathankw@stanford.edu"
 
 description = ""
 parser = ArgumentParser(description=description)
-parser.add_argument('-i',required=True,help="The tab-delimited input file specifying which paired-end FASTQ files to map. The columns are: 1) path to read1, 2) path to read2, 3) path to output directory.")
+parser.add_argument('-i',required=True,help="The tab-delimited input file specifying which paired-end FASTQ files to map. The columns are: 1) path to read1, 2) path to read2, 3) name of the output same file. Must have a .sam extension..")
 
 args = parser.parse_args()
 
@@ -17,13 +17,18 @@ for line in fh:
 	line = line.strip("\n")
 	if not line:
 		continue
-	read1,read2,outdir = line.split("\t")
+	read1,read2,sam = line.split("\t")
 	for i in read1,read2:
 		if not os.path.exists(i):
 			raise Exception("FASTQ file {i} does not exist!")
 	barcode = read1.rsplit("_",3)[-3]
-	sjmFile = os.path.join(outdir,"map_" + barcode + ".sjm")
-	cmd = "jsonWorkflow.py -c " + os.getenv("JSONWF_INSTANCES") + "--run -s " + sjmFile + " --mail-to " + MAIL_TO
-	cmd += " read1={read1} read2={read2}".format(read1=read1,read2=read2)
-	subprocess.Popen(cmd,shell=True)
+	sjmFile = sam.rstrip(".sam") + ".sjm"
+	outdir = os.path.dirname(sjmFile)
+	confFile = os.path.join(os.getenv("JSONWF_INSTANCES"),"bwa-mem.json")
+	cmd = "jsonWorkflow.py -c " + confFile  + " -s " + sjmFile + " --mail-to " + MAIL_TO + " --outdir " + outdir
+	#cmd += " --run"
+	cmd += " read1={read1} read2={read2} samFile={sam}".format(read1=read1,read2=read2,sam=sam)
+	print("Running command {cmd}".format(cmd=cmd))
+	break
+	#subprocess.Popen(cmd,shell=True)
 		
