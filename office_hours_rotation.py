75cc6fc98e9ed86c6f8ada9da9e1892c8e34a71e
Nathaniel Watson <nathankw@stanford.edu>
2016-08-31 17:19:56 -0700

    Added office hours rotation script

---
+++ office_hours_rotation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import itertools
+import random
+
+def print_rotation(rotation):
+	for i in rotation:
+		print("{}\t{}".format(i[0],i[1]))
+
+names = ['Ramesh', 'Watson', 'Paul', 'Isaac', 'Amin', 'Keith']
+if len(names) <4:
+	raise Exception("There aren't enough person in the names list for this rotation generator to work.")
+
+gen = itertools.combinations(names,2)
+combs = list(gen)
+random.shuffle(combs)
+
+rotation = []
+start = random.sample(combs,1)[0]
+combs.remove(start)
+rotation.append(start)
+
+while combs:
+	for i in range(len(combs)):
+		pair = combs[i]
+		if (pair[0] in rotation[-1]) or (pair[1] in rotation[-1]):
+			if i != len(combs) -1:
+				continue
+		combs.remove(pair)
+		rotation.append(pair)
+		break
+	print(combs)
+
+print_rotation(rotation)
+	
