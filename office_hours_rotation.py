77e4dc3deb10e22bfee04aa5713c2d0d56fa5881
Nathaniel Watson <nathankw@stanford.edu>
2017-10-25 14:47:02 -0700

    Added version check

--- office_hours_rotation.py
+++ office_hours_rotation.py
@@ -8,7 +8,17 @@
 
 import itertools
 import random
+import sys
 from argparse import ArgumentParser
+
+#Make sure version of Python is >= 2.7.9
+python_version = sys.version_info
+version_err_msg = "You must be using a version of Python >= 2.7 to use this script."
+if type(python_version) is tuple: #.i.e Python 2.6
+		raise Exception(version_err_msg)
+elif python_version.major == 2:
+	if python_version.minor < 7:
+		raise Exception(version_err_msg)
 
 description = "Generates a rotation list for SCG Office Hours. Given the list of office hours representatives (stored in NAMES), uses the random.combination() method to select all combinations of size two from NAMES. Then, tries to randomize the rotation list by requiring that no two consecutive office hour sessions  have the same representative in both."
 parser = ArgumentParser(description=description)
@@ -21,7 +31,7 @@ def print_rotation(rotation):
 	for i in rotation:
 		print("{}\t{}".format(i[0],i[1]))
 
-NAMES = ["Ramesh", "Watson", "Paul", "Amir", "Keith", "Wendy", "Vandhana", "Priya"]
+NAMES = ["Ramesh", "Gao", "Watson", "Paul", "Amir", "Keith", "Wendy", "Vandhana", "Priya"]
 if len(NAMES) <4:
 	raise Exception("There aren't enough names in the NAMES list for this rotation generator to work.")
 
